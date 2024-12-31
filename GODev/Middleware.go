0ef11b6bda57c3dcf7c3e38a891c793eb41bdc0d
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-31 18:12:25 +0530

    Middelwares

--- GODev/Middleware.go
+++ GODev/Middleware.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// log.Println(r.URL.Path)
-		// f(w, r)
+		log.Println(r.URL.Path)
+		f(w, r)
 	}
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintln(w, "foo")
+	fmt.Fprintln(w, "foo")
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/GOTUT
 
 go 1.23.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
